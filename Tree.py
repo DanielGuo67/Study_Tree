669cc936deed72a7910eedb66c98d3b7b33e28da
DanielGuo67 <53518725+DanielGuo67@users.noreply.github.com>
2020-07-28 18:11:47 +0800

    2020-07-29 18:10 Tree study
    Version one

---
+++ Tree.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from math import log
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]                      #遍历列表中dataSet的每一行，featVec[-1]是这一行的最后一列的值
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+            labelCounts[currentLabel] += 1
+        shannonEnt = 0.0
+        for key in labelCounts:
+            prob = float(labelCounts[key]/numEntries)
+            shannonEnt -= prob * log(prob,2)
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [
+        [1,1,'yes'],
+        [1,1,'yes'],
+        [1,0,'no'],
+        [0,1,'no'],
+        [0,1,'no']
+    ]
+    labels = ['no surfacing','flippers']
+    return dataSet,labels
+
+
+if __name__ == '__main__':
+    g1,l1 = createDataSet()
+    print(calcShannonEnt(g1))
+
